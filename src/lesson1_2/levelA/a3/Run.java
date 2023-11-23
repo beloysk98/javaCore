345bebafb262aea6a3e57b829951f07f05d69fe3
Vlad <vladbeloysk@gmail.com>
2023-11-23 14:48:38 +0300

    Добавил решение задачи урока 1_2, а3

---
+++ src/lesson1_2/levelA/a3/Run.java
@@ -0,0 +1,11 @@
+package javaCore.src.lesson1_2.levelA.a3;
+/*
+В методе main класса Run создайте константу которая будет хранить в себе номер
+месяца вашего рождения. Выведите значение константы в консоль.
+ */
+public class Run {
+    public static void main(String[] args) {
+        final byte MONTH = 8;
+        System.out.println("Месяц вашего рождения: "+MONTH);
+    }
+}
