4d48d05e4f59a448253144b33478e0e25546fbd5
Vlad <vladbeloysk@gmail.com>
2023-11-23 15:28:42 +0300

    решение задачи a3 урока 1_3

---
+++ src/lesson1_3/levelA/a3/Run.java
@@ -0,0 +1,12 @@
+package javaCore.src.lesson1_3.levelA.a3;
+/*
+Создать две переменные типа int и проинициализировать их значениями 7 и 5.
+ Выведите в консоль остаток от деления первого числа на второе.
+ */
+public class Run {
+    public static void main(String[] args) {
+        int var1=7;
+        int var2=5;
+        System.out.println("Остаток от деления: "+(var1%var2));
+    }
+}
