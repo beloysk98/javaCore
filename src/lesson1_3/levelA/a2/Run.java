8cfe91606249d6ae1a3681d9c92303f92b03bb29
Vlad <vladbeloysk@gmail.com>
2023-11-23 15:22:28 +0300

    решение задачи a2 урока 1_3

---
+++ src/lesson1_3/levelA/a2/Run.java
@@ -0,0 +1,13 @@
+package javaCore.src.lesson1_3.levelA.a2;
+
+/*
+Создать переменную типа byte и присвоить ей значение 5.
+Используя пост-унарный оператор увеличьте значение переменной на единицу и выведите результат в консоль.
+ */
+public class Run {
+    public static void main(String[] args) {
+        byte var = 5;
+        var++;
+        System.out.println(var);
+    }
+}
