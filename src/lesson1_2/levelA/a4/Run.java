95a504b4c4a6f3d475207c63bace3105b966c0cc
Vlad <vladbeloysk@gmail.com>
2023-11-23 15:10:31 +0300

    Добавил решение задачи урока 1_2, а4

---
+++ src/lesson1_2/levelA/a4/Run.java
@@ -0,0 +1,17 @@
+package javaCore.src.lesson1_2.levelA.a4;
+
+/*
+Создайте enum класс дней недели.
+ */
+public class Run {
+    public enum DayOfWeek {
+
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY
+    }
+}
