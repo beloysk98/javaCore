380ec5c750512d2510f321a18adb3ca463600adb
Vlad <vladbeloysk@gmail.com>
2023-11-29 14:23:40 +0300

    решение задачи урока 1_2 levleB b2

---
+++ src/lesson1_2/levelB/b2/Run.java
@@ -0,0 +1,16 @@
+package javaCore.src.lesson1_2.levelB.b2;
+
+/*
+В методе main класса Run создайте переменную а типа int со значением 1 и переменную b типа int со значением 2.
+ Выведите в консоль результат математической операции a/b. Сверьте результат из консоли с калькулятором,
+  он должен быть одинаковым!
+ */
+public class Run {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        double c=(double) a/b;
+        System.out.println(c);
+
+    }
+}
