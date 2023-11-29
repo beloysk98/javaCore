64988ab07ac972d9706d9ef7f796dd655577b36a
Vlad <vladbeloysk@gmail.com>
2023-11-29 14:05:52 +0300

    исправление решения урок 1_3 b3

--- src/lesson1_3/levelB/b3/Run.java
+++ src/lesson1_3/levelB/b3/Run.java
@@ -11,12 +11,13 @@ public class Run {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите длину первого катета: ");
         // в строчке ниже у нас просто неизвестная переменная или мы более точно знаем что там хранится?
-        int var1 = scan.nextInt();
+        // мы знаем что будет целое число типа int . так как при вводе другого типа будет ошибка
+        int lengthA = scan.nextInt();
         // может дать более поятное название чтобы понимать что туда сохранили?
         System.out.print("Введите длину второго катета: ");
-        int var2 = scan.nextInt();
-        double varKor1 = Math.pow(var1, 2);
-        double varKor2 = Math.pow(var2, 2);
+        int lengthB = scan.nextInt();
+        double varKor1 = Math.pow(lengthA, 2);
+        double varKor2 = Math.pow(lengthB, 2);
         double varSum = varKor1 + varKor2;
         double varHip = Math.sqrt(varSum);
         System.out.println("Длина гипотенузы равна: " + varHip);
