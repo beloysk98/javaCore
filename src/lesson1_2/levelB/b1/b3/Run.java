329f4e5b6ad3e6ed7ceb794f2e2fc8bcfe783831
Vlad <vladbeloysk@gmail.com>
2023-11-29 14:25:16 +0300

    решение задачи урока 1_2 levleB b3

---
+++ src/lesson1_2/levelB/b1/b3/Run.java
@@ -0,0 +1,10 @@
+package javaCore.src.lesson1_2.levelB.b1.b3;
+/*
+В методе main класса Run написать код, который выводит в консоль значение числа “Пи”.
+ Использовать константу из jdk.
+ */
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Число Пи: "+Math.PI);
+    }
+}
