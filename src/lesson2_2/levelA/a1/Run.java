16a905635686cccf8650b0e517e1bd525993b3d2
Vlad <vladbeloysk@gmail.com>
2023-12-01 16:39:52 +0300

    урок 2_2 a1 решение задачи

---
+++ src/lesson2_2/levelA/a1/Run.java
@@ -0,0 +1,14 @@
+package javaCore.src.lesson2_2.levelA.a1;
+
+/*
+В методе main класса Run используя цикл for выведите в консоль числа
+ от 1 до 10.
+
+ */
+public class Run {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
