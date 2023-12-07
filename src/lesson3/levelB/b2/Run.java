3fd697b1a47a6122526d64c7d8e92df5f5fa25e3
Vlad <vladbeloysk@gmail.com>
2023-12-07 22:12:44 +0300

    добавление решения задачи

---
+++ src/lesson3/levelB/b2/Run.java
@@ -0,0 +1,20 @@
+package javaCore.src.lesson3.levelB.b2;
+
+/*
+Создайте массив из 10 элементов со случайными числами от 0 до 9.
+ Напишите код для нахождения суммы массива чисел. Выведите в консоль сумму массива.
+ */
+public class Run {
+    public static void main(String[] args) {
+        int value = 0;
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.print(array[i] + ",");
+            value = value + array[i];
+        }
+        System.out.println();
+        System.out.println("Сумма: " + value);
+
+    }
+}
