2ea1a61424b917a6a459158435baba3e72a19511
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-12-12 16:15:20 +0300

    замечание к 3В2

--- src/lesson3/levelB/b2/Run.java
+++ src/lesson3/levelB/b2/Run.java
@@ -6,7 +6,7 @@ package javaCore.src.lesson3.levelB.b2;
  */
 public class Run {
     public static void main(String[] args) {
-        int value = 0;
+        int value = 0; // а что хранится в этой переменной? как её лучше назвать, чтобы название говорило само за себя?
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10);
