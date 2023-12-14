0bc4345146d036d1c671d1aa09ef5423f3ad5d1e
Vlad <vladbeloysk@gmail.com>
2023-12-14 16:34:33 +0300

    удаление лишнего текста, замена пробелов на символ \t(равен четырем пробелам).

--- src/lesson3/levelC/c1/Run.java
+++ src/lesson3/levelC/c1/Run.java
@@ -7,11 +7,10 @@ package javaCore.src.lesson3.levelC.c1;
 public class Run {
     public static void main(String[] args) {
         int[][] twoArray = new int[4][5];
-        //int [][] twoArray= new int {{1,2,3,4,5}{6,7,8,9,10}{11,12,13,14,15}{16,17,18,19,20}}
         for (int i = 0; i < twoArray.length; i++) {
             for (int j = 0; j < twoArray[0].length; j++) {
                 twoArray[i][j] = (int) (Math.random() * 100);
-                System.out.print("для какой красоты этот пробел был здесь?) посмотри что делает \t " + twoArray[i][j] + " ");
+                System.out.print("\t" + twoArray[i][j] + "\t");
             }
             System.out.println();
         }
