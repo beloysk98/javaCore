7ec22f99b3234682b4884fc9bf442ae4712c71bc
Vlad <vladbeloysk@gmail.com>
2023-12-14 17:11:42 +0300

    исправление ошибок. Переименование переменных. удаление лишнего текста

--- src/lesson2_2/levelB/b4/Run.java
+++ src/lesson2_2/levelB/b4/Run.java
@@ -9,21 +9,20 @@ import java.util.Scanner;
 public class Run {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int value0 = scan.nextInt(); // value0 - это плохое не информативное имя. переименуй его в название из имени которого будет понятно что там хранится.
-        int temp = 2; // для чего тут переменной temp присваивается число 2?
+        int inputValue = scan.nextInt();
+        int temp;
         boolean prostoe = true;
-        for (int i = 2; i <= value0 / 2; i++) {
-            temp = value0 % i;
+        for (int i = 2; i <= inputValue / 2; i++) {
+            temp = inputValue % i;
             if (temp == 0) {
                 prostoe = false;
                 break;
             }
         }
         if (prostoe) {
-            System.out.println(value0 + " - простое число");
+            System.out.println(inputValue + " - простое число");
         } else {
-            System.out.println(value0 + " - составное число");
+            System.out.println(inputValue + " - составное число");
         }
-
     }
 }
