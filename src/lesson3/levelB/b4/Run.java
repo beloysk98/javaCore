9b5cb83aa5363228f31041c78705e4083e3711fb
Vlad <vladbeloysk@gmail.com>
2023-12-14 17:24:46 +0300

    замена типа данных с int-> double  среднеарифметического значения, для правильного вывода ответа

--- src/lesson3/levelB/b4/Run.java
+++ src/lesson3/levelB/b4/Run.java
@@ -7,18 +7,18 @@ package javaCore.src.lesson3.levelB.b4;
  */
 public class Run {
     public static void main(String[] args) {
-        int sum = 0;
-        int average;
+        double sum = 0;
+        double average;
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10);
             sum = array[i] + sum;
         }
         average = sum / array.length;
-        System.out.print("Элементы меньше среднего арифметического= ");
+        System.out.print("Элементы меньше среднего арифметического = ");
         for (int i : array) {
             if (i < average) {
-                System.out.print(i + ",");
+                System.out.print(i + " ");
             }
         }
         // для 0 2 7 3 2 7 9 4 1 9   average = 4.4
