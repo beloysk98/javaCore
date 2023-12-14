447569ddb51596cccdfc830c598f1a574335b222
Vlad <vladbeloysk@gmail.com>
2023-12-14 17:28:21 +0300

    переименнование переменной , для большего понимания ее значения

--- src/lesson3/levelB/b2/Run.java
+++ src/lesson3/levelB/b2/Run.java
@@ -1,20 +1,18 @@
 package javaCore.src.lesson3.levelB.b2;
-
 /*
 Создайте массив из 10 элементов со случайными числами от 0 до 9.
  Напишите код для нахождения суммы массива чисел. Выведите в консоль сумму массива.
  */
 public class Run {
     public static void main(String[] args) {
-        int value = 0; // а что хранится в этой переменной? как её лучше назвать, чтобы название говорило само за себя?
+        int sum = 0;
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10);
             System.out.print(array[i] + ",");
-            value = value + array[i];
+            sum = sum + array[i];
         }
         System.out.println();
-        System.out.println("Сумма: " + value);
-
+        System.out.println("Сумма: " + sum);
     }
 }
