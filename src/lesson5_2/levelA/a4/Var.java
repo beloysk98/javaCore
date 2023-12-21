81386b52e9cc77c56c32e61a5123e1a6090fea48
Vlad <vladbeloysk@gmail.com>
2023-12-21 17:58:57 +0300

    создание обстрактного класса и его наследников

---
+++ src/lesson5_2/levelA/a4/Var.java
@@ -0,0 +1,18 @@
+package javaCore.src.lesson5_2.levelA.a4;
+
+/*
+Создайте классы Scalar, Vector, Matrix, которые наследуются от абстрактного класса Var.
+ */
+public abstract class Var {
+
+}
+
+class Scalar extends Var {
+}
+
+class Vector extends Var {
+}
+
+class Matrix extends Var {
+
+}
