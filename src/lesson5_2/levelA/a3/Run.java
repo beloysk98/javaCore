7accc190f89c5fe4c9bf6b96179574131aad2445
Vlad <vladbeloysk@gmail.com>
2023-12-24 13:40:42 +0300

    создание обьектов классов наследников Animal

--- src/lesson5_2/levelA/a3/Run.java
+++ src/lesson5_2/levelA/a3/Run.java
@@ -2,11 +2,11 @@ package javaCore.src.lesson5_2.levelA.a3;
 
 public class Run {
     public static void main(String[] args) {
-        fish carp = new fish(); //название рыбы
-        carp.voice();
-        fishFamily carpFamily = new fishFamily();//семейство рыб
-        carpFamily.voice();
-        squads carpLike = new squads();//карпообразные
-        carpLike.voice();
+        multicellular seaweed = new multicellular();
+        seaweed.voice();
+        chordates ascidia = new chordates();
+        ascidia.voice();
+        Vertebrates fish = new Vertebrates();
+        fish.voice();
     }
 }
