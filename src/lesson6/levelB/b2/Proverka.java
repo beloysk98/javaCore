f4bc35fe8f0bd74aaca2490948a8533771b705c4
Vlad <vladbeloysk@gmail.com>
2023-12-28 19:02:12 +0300

    изменение регулярного выражения

--- src/lesson6/levelB/b2/Proverka.java
+++ src/lesson6/levelB/b2/Proverka.java
@@ -1,11 +1,14 @@
 package javaCore.src.lesson6.levelB.b2;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Proverka {
+    Scanner scan = new Scanner(System.in);
     void proverka() {
-        String inputText = "apple, banana, cherry ada-ae 3.2 - dad! dakd dla d.Dad da . ";
-        String[] arrText = inputText.split("[^\\p{L}-]|(?<!\\w)-|-(?!\\w)");
+        System.out.print("Введите текст: ");
+        String inputText = scan.nextLine();
+        String[] arrText = inputText.split("(\\s-)|[0-9,.\\s?!;:()<>]");
         arrText = Arrays.stream(arrText).filter(s -> !s.isEmpty()).toArray(String[]::new);
         System.out.println(arrText.length);
 
