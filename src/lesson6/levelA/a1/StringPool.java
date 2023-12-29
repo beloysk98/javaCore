c637b8ce220c5e8036731bd87cff8dadcfd4cc5e
Nesterovich Alexsander <nesterovich.aleksandr.a@gmail.com>
2023-12-29 10:59:18 +0300

    вопрос к 6А1

--- src/lesson6/levelA/a1/StringPool.java
+++ src/lesson6/levelA/a1/StringPool.java
@@ -8,6 +8,8 @@ package javaCore.src.lesson6.levelA.a1;
 Ответ : 3 строки будет храниться, так как при создании обьекта через оператор new строка не помещается в пул строк
 В переменной s4 при помощи метода intern() мы помещаем строку s3 в пул и присваеваем ее значение s4
 
+Александр: Какие строки храниятся в String Pool?
+
  */
 public class StringPool {
     public static void main(String[] args) {
