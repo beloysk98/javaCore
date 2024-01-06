fec44ac327762669668938e35b2d5dc80565a875
Vlad <vladbeloysk@gmail.com>
2024-01-06 10:55:15 +0300

    изменение решения задачи, оптимизация кода

--- src/lesson6/levelB/b4/Proverka.java
+++ src/lesson6/levelB/b4/Proverka.java
@@ -1,79 +1,26 @@
 package javaCore.src.lesson6.levelB.b4;
 
-import java.util.Scanner;
-/*
-Velcom A1 +375 29 или 8029 1/3/6/9
-          +375 44 или 8044 все номера
-MTC         +375 29 или 8029 2/5/7/8
-            +375 33 или 8033 все номера
-Life        +375 25 или 8025 все номера
-
-Городской(Бел телеком) +375 17 или 8017 все номера
-
- */
-
+//в 26 влез только
 public class Proverka {
-
-    // а теперь подумай как сделать так, чтобы в одном методе был оне больше 20 строк.
-    // есть такое правило: если весь код в методе не влазит на экран, значит в этом методе находится говнокод)
-    void operator() {
-        System.out.println("Введите номер для проверки: ");
-        String inputNumber = new Scanner(System.in).nextLine();
+    void operator(String inputNumber) {
         String rep = inputNumber.replaceAll("(-)|[+\\s()a-zA-Zа-яА-Я.>;:'\"<\\[,!*?^$/&%\\]{}|]", "");
-        String srav = rep.substring(0, 2);
-        if (srav.equals("80")) {
-            String repNew23 = rep.substring(2, 4);
-            switch (repNew23) {
-                case "17":
-                    System.out.println("Оператор белТелеком");
-                    break;
-                case "33":
-                    System.out.println("Оператор MTC");
-                    break;
-                case "29":
-                    String repNew232 = rep.substring(5);
-                    // 2/5/7/8
-                    if (repNew232.equals("2") || repNew232.equals("5") || repNew232.equals("7") || repNew232.equals("8")) {
-                        System.out.println("Оператор MTC");
-                        //1/3/6/9
-                    } else {
-                        System.out.println("Оператор Velcome");
-                    }
-                    break;
-                case "25":
-                    System.out.println("Оператор life");
-                    break;
-                case "44":
-                    System.out.println("Оператор Velcome");
-                    break;
+        String repNew24 = rep.substring(2, 4);
+        String repNew35 = rep.substring(3, 5);
+        String repNew56 = rep.substring(5, 6);
+        if (repNew24.equals("17") || repNew35.equals("17")) {
+            System.out.println("Оператор белТелеком");
+        } else if (repNew24.equals("29") || repNew35.equals("29")) {
+            if (repNew56.equals("2") || repNew56.equals("5") || repNew56.equals("7") || repNew56.equals("8")) {
+                System.out.println("Оператор MTC");
+            } else {
+                System.out.println("Оператор Velcome");
             }
+        } else if (repNew24.equals("33") || repNew35.equals("33")) {
+            System.out.println("Оператор MTC");
+        } else if (repNew24.equals("44") || repNew35.equals("44")) {
+            System.out.println("Оператор Velcome");
         } else {
-            String repNew = rep.substring(3, 5);
-            switch (repNew) {
-                case "17":
-                    System.out.println("Оператор белТелеком");
-                    break;
-                case "33":
-                    System.out.println("Оператор MTC");
-                    break;
-                case "29":
-                    String repNew232 = rep.substring(5, 6);
-                    // 2/5/7/8
-                    if (repNew232.equals("2") || repNew232.equals("5") || repNew232.equals("7") || repNew232.equals("8")) {
-                        System.out.println("Оператор MTC");
-                        //1/3/6/9
-                    } else {
-                        System.out.println("Оператор Velcome");
-                    }
-                    break;
-                case "25":
-                    System.out.println("Оператор life");
-                    break;
-                case "44":
-                    System.out.println("Оператор Velcome");
-                    break;
-            }
-
+            System.out.println("Такого оператора РБ не существует");
         }
     }
 }
--- src/lesson6/levelB/b4/Run.java
+++ src/lesson6/levelB/b4/Run.java
@@ -1,5 +1,7 @@
 package javaCore.src.lesson6.levelB.b4;
 
+import java.util.Scanner;
+
 /*
 Напишите программу для определения оператора РБ по номеру телефона.
  Создать класс Run и в методе main проверить работу этой программы.
@@ -7,6 +9,11 @@ package javaCore.src.lesson6.levelB.b4;
 public class Run {
     public static void main(String[] args) {
         Proverka prov = new Proverka();
-        prov.operator();
+        prov.operator(inputNumber());
+    }
+
+    private static String inputNumber() {
+        System.out.println("Введите номер для проверки: ");
+        return new Scanner(System.in).nextLine();
     }
 }
