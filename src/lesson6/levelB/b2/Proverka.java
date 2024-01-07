82b62f31473385f11be4ab9a1ee4e69f8fd24148
Vlad <vladbeloysk@gmail.com>
2024-01-07 10:59:35 +0300

    первая итерация рефакторинга. вынос второстепенных проверок в отдельные методы. упрощение понятия кода

--- src/lesson6/levelB/b2/Proverka.java
+++ src/lesson6/levelB/b2/Proverka.java
@@ -4,15 +4,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Proverka {
-    Scanner scan = new Scanner(System.in);
     void proverka() {
+        String inputString = inputString();
+        String[] textDivision = textDivision(inputString);
+        String[] deleteSpaces = deleteSpaces(textDivision);
+        print(deleteSpaces);
+    }
+
+    private void print(String[] inStr) {
+        System.out.println(inStr.length);
+    }
+
+    private String[] deleteSpaces(String[] inStr) {
+        return Arrays.stream(inStr).filter(s -> !s.isEmpty()).toArray(String[]::new);
+    }
+
+    private String[] textDivision(String inStr) {
+        return inStr.split("(\\s-)|[0-9,.\\s?!;:()<>]");
+    }
+
+    private String inputString() {
         System.out.print("Введите текст: ");
-        String inputText = scan.nextLine();
-        String[] arrText = inputText.split("(\\s-)|[0-9,.\\s?!;:()<>]");
-        arrText = Arrays.stream(arrText).filter(s -> !s.isEmpty()).toArray(String[]::new);
-        System.out.println(arrText.length);
-
-
+        return new Scanner(System.in).nextLine();
     }
 }
 
