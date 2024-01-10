ad32c89ac300be14e93a326c29498ef146c72f16
Vlad <vladbeloysk@gmail.com>
2024-01-10 19:12:55 +0300

    Рефакторинг кода. обьеденинение методов , реализация их в одном методе

--- src/lesson6/levelB/b1/TestPolindrom.java
+++ src/lesson6/levelB/b1/TestPolindrom.java
@@ -3,54 +3,18 @@ package javaCore.src.lesson6.levelB.b1;
 import java.util.Scanner;
 
 public class TestPolindrom {
-    private String replaceSpaces;
-    private String reversalLine;
     void print() {
-        String inputString = inputString();
-        String toUpLetters = toUpLetters(inputString);
-        replaceSpaces = replaceSpaces(toUpLetters);
-        reversalLine = reversalLine(replaceSpaces);
-        proverka();
-    }
-
-    private String toUpLetters(String toUp) {
-        return toUp.toUpperCase();
-    }
-
-    private void proverka() {
-        if (isPolindrom()) {
-            thisPolindrom();
-        } else {
-            thisNotPolindrom();
-        }
-
-    }
-
-    private void thisNotPolindrom() {
-        System.out.println("Слово/словосочетание не полиндром");
-    }
-
-    private void thisPolindrom() {
-        System.out.println("Слово/словосочетание полиндром");
-    }
-
-    private boolean isPolindrom() {
-        return replaceSpaces.equals(reversalLine);
-    }
-
-    private String reversalLine(String str) {
-        StringBuilder rever = new StringBuilder(str);
-        rever.reverse();
-        return rever.toString();
-    }
-
-    private String replaceSpaces(String inStr) {
-        return inStr.replaceAll(" ", "");
-    }
-
-    private String inputString() {
         System.out.print("Введите слово для проверки: ");
-        return new Scanner(System.in).nextLine();
+        String inputString = new Scanner(System.in).nextLine();
+        String toUp = inputString.toUpperCase();
+        String replaceSpaces = toUp.replaceAll(" ", "");
+        StringBuilder rever = new StringBuilder(replaceSpaces);
+        String reversalLine = rever.toString();
+        if (replaceSpaces.equals(reversalLine)) {
+            System.out.println("Слово/словосочетание полиндром");
+        } else {
+            System.out.println("Слово/словосочетание не полиндром");
+        }
     }
 }
 
