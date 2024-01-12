ef3540d58418f4feb3f8621359921e00c57d275e
Vlad <vladbeloysk@gmail.com>
2024-01-12 09:06:33 +0300

    Создание внутренего класса. изменение порядка методов, рефакторинг классов

--- src/lesson7/levelA/a1/University.java
+++ src/lesson7/levelA/a1/University.java
@@ -9,6 +9,42 @@ package javaCore.src.lesson7.levelA.a1;
 public class University {
     Student igor = new Student();
 
+    public class Student {
+        private String name;
+        private int age;
+
+        private class InfoStudent {
+            public void infoIgor() {
+                setName("Игорь");
+                setAge(26);
+                System.out.println("Имя: " + getName());
+                System.out.println("Возраст: " + getAge());
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void printInfoStudent() {
+            InfoStudent infoStudent = new InfoStudent();
+            infoStudent.infoIgor();
+        }
+    }
+
+
     public void printInfo() {
         igor.printInfoStudent();
     }
